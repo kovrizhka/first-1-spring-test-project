cf2f9e58dc5ab020a1d1479336445ebbd4d61387
kovrizhka <pivsru@gmail.com>
2023-05-21 13:04:58 +0500

    using Java annotations: @Scope @Value @PostConstruct @PreDestroy

--- src/main/java/ru/kovrizhkin/springlearn/ClassicalMusic.java
+++ src/main/java/ru/kovrizhkin/springlearn/ClassicalMusic.java
@@ -2,28 +2,27 @@ package ru.kovrizhkin.springlearn;
 
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import java.util.ArrayList;
 
 @Component
 public class ClassicalMusic implements Music{
+    @PostConstruct
+    public void doMyInit() {
+        System.out.println("Im initializing...");
+    }
+
+    @PreDestroy
+    public void doMyDestroy() {
+        System.out.println("Im destroying :(");
+    }
 
     private ArrayList<String> classicalMusicList = new ArrayList<>() {{
         add("Magic Flute");
         add("Rhythm Of The Rain");
         add("Love Story");
     }};
-
-//    public static ClassicalMusic getClassicalMusic() {
-//        return new ClassicalMusic();
-//    }
-//
-//    public void doMyInit() {
-//        System.out.println("Initialization in progress...");
-//    }
-//
-//    public void doMyDestroy() {
-//        System.out.println("I'm destroying");
-//    }
     @Override
     public ArrayList<String> getSong() {
         return this.classicalMusicList;
--- src/main/java/ru/kovrizhkin/springlearn/TestSpring.java
+++ src/main/java/ru/kovrizhkin/springlearn/TestSpring.java
@@ -8,53 +8,12 @@ public class TestSpring {
                 "applicationContext.xml"
         );
 
-        Computer computer = context.getBean("computer", Computer.class);
-        System.out.println(computer);
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        System.out.println(musicPlayer.getName());
+        System.out.println(musicPlayer.getVolume());
 
-//        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-//        musicPlayer.playMusic();
+        ClassicalMusic classicalMusic1 = context.getBean("classicalMusic", ClassicalMusic.class);
 
-        /**
-         * Injection by @Component
-         */
-//        Music music = context.getBean("rockMusic", Music.class);
-//
-//        MusicPlayer musicPlayer = new MusicPlayer(music);
-//
-//        Music music1 = context.getBean("tranceMusic", Music.class);
-//
-//        MusicPlayer tranceMusicPlayer = new MusicPlayer(music1);
-//
-//        tranceMusicPlayer.playMusic();
-//        musicPlayer.playMusic();
-
-
-
-
-//        ClassicalMusic classicalMusic = context.getBean("classicalBean", ClassicalMusic.class);
-//        System.out.println(classicalMusic.getSong());
-//        System.out.println();
-//        ClassicalMusic classicalMusic1 = context.getBean("classicalBean", ClassicalMusic.class);
-//        System.out.println(classicalMusic1.getSong());
-
-
-//        MusicPlayer musicPlayer1 = context.getBean("musicPlayer", MusicPlayer.class);
-//        MusicPlayer musicPlayer2 = context.getBean("musicPlayer", MusicPlayer.class);
-//
-//        boolean comparison = musicPlayer1 == musicPlayer2;
-//
-//        System.out.println(comparison);
-//
-//        System.out.println(musicPlayer1);
-//        System.out.println(musicPlayer2);
-//
-//        musicPlayer1.setVolume(1245);
-//
-//        System.out.println(musicPlayer1.getVolume());
-//        System.out.println(musicPlayer2.getVolume());
-
-//        System.out.println(musicPlayer.getName());
-//        System.out.println(musicPlayer.getVolume());
 
         context.close();
     }
